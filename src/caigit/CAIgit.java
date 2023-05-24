218a4c5c5ac1fd127587eaf7bd232eb6045f970e
unknown <marcelo11912@hotmail.com>
2023-05-23 22:04:35 -0500

    Creando mi proyecto

---
+++ src/caigit/CAIgit.java
@@ -0,0 +1,12 @@
+package caigit;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class CAIgit {
+
+    private static final int PORCENTAJE_APROBACION = 75;
+
+    public static void main(String[] args) {
+       
+}
